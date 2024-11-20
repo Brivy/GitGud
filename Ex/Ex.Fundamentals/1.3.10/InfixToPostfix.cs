6fec55eb9e1a10d43abf101ebdb7c352458001f5
Brian van Yperen <>
2024-11-20 08:28:39 +0100

    Shunting-yard algorithm

--- Ex/Ex.Fundamentals/1.3.10/InfixToPostfix.cs
+++ Ex/Ex.Fundamentals/1.3.10/InfixToPostfix.cs
@@ -1,5 +1,4 @@
-﻿using GitGud.DS.Stack.Implementations;
-using System.Text;
+﻿using System.Text;
 
 namespace GitGud.Ex.Fundamentals._1._3._10;
 
@@ -7,39 +6,96 @@ internal static class InfixToPostfix
 {
     public static string Parse(string expression)
     {
-        var operators = new LinkedListStack<char>();
-        var digits = new LinkedListStack<char>();
+        var operators = new Stack<char>();
+        var output = new StringBuilder();
+
         foreach (var character in expression)
         {
-            if (character is '+' or '-' or '*' or '/') operators.Push(character);
-            else if (char.IsDigit(character)) digits.Push(character);
-        }
-
-        var postfixStack = new LinkedListStack<string>();
-        while (!operators.IsEmpty())
-        {
-            var primaryOperator = operators.Pop();
-            if (primaryOperator is '*' or '/' && operators.Size() >= 1)
+            if (char.IsDigit(character))
             {
-                var secondaryOperator = operators.Pop();
-                var rightDigit = digits.Pop();
-                var leftDigit = digits.Pop();
-                postfixStack.Push($"{leftDigit} {rightDigit} {primaryOperator} {secondaryOperator}");
+                output.Append(character).Append(' ');
             }
-            else
+            else if (character is '+' or '-' or '*' or '/')
             {
-                var digit = digits.Pop();
-                postfixStack.Push($"{digit} {primaryOperator}");
+                while (operators.Count > 0 && Precedence(operators.Peek()) >= Precedence(character))
+                {
+                    output.Append(operators.Pop()).Append(' ');
+                }
+                operators.Push(character);
             }
         }
 
-        var postfix = new StringBuilder($"{digits.Pop()}");
-        while (!postfixStack.IsEmpty())
+        while (operators.Count > 0)
         {
-            var postfixPart = postfixStack.Pop();
-            postfix.Append($" {postfixPart}");
+            output.Append(operators.Pop()).Append(' ');
         }
 
-        return postfix.ToString();
+        return output.ToString().TrimEnd();
     }
+
+    private static int Precedence(char op)
+    {
+        return op switch
+        {
+            '+' or '-' => 1,
+            '*' or '/' => 2,
+            _ => 0
+        };
+    }
+
+
+    //public static string Parse(string expression)
+    //{
+    //    var operators = new LinkedListStack<char>();
+    //    var digits = new LinkedListStack<char>();
+    //    foreach (var character in expression)
+    //    {
+    //        if (character is '+' or '-' or '*' or '/') operators.Push(character);
+    //        else if (char.IsDigit(character)) digits.Push(character);
+    //    }
+
+    //    var postfixStack = new LinkedListStack<string>();
+    //    while (!operators.IsEmpty())
+    //    {
+    //        var primaryOperator = operators.Pop();
+    //        if (primaryOperator is '*' or '/' && operators.Size() >= 1)
+    //        {
+    //            var secondaryOperator = operators.Pop();
+    //            if (secondaryOperator is '*' or '/')
+    //            {
+    //                var digit = digits.Pop();
+    //                postfixStack.Push($"{digit} {primaryOperator}");
+    //                while (secondaryOperator is '*' or '/')
+    //                {
+    //                    digit = digits.Pop();
+    //                    postfixStack.Push($"{digit} {secondaryOperator}");
+    //                    secondaryOperator = operators.Pop();
+    //                }
+    //                var leftDigit = digits.Pop();
+    //                var finalPostfix = postfixStack.Pop();
+    //                postfixStack.Push($"{leftDigit} {finalPostfix} {secondaryOperator}");
+    //            }
+    //            else
+    //            {
+    //                var rightDigit = digits.Pop();
+    //                var leftDigit = digits.Pop();
+    //                postfixStack.Push($"{leftDigit} {rightDigit} {primaryOperator} {secondaryOperator}");
+    //            }
+    //        }
+    //        else
+    //        {
+    //            var digit = digits.Pop();
+    //            postfixStack.Push($"{digit} {primaryOperator}");
+    //        }
+    //    }
+
+    //    var postfix = new StringBuilder($"{digits.Pop()}");
+    //    while (!postfixStack.IsEmpty())
+    //    {
+    //        var postfixPart = postfixStack.Pop();
+    //        postfix.Append($" {postfixPart}");
+    //    }
+
+    //    return postfix.ToString();
+    //}
 }
