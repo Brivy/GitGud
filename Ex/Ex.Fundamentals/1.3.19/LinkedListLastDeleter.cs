4a14bce2b6363fa06cc3bd2374946ba7a0ebc8e2
Brian van Yperen <brianyperen@gmail.com>
2025-02-27 13:45:09 +0100

    1.3.19

---
+++ Ex/Ex.Fundamentals/1.3.19/LinkedListLastDeleter.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+
+namespace GitGud.Ex.Fundamentals._1._3._19;
+
+public class LinkedListLastDeleter<TItem> : IEnumerable<TItem>
+{
+    private Node? _first;
+    private int _index;
+
+    private sealed class Node
+    {
+        public required TItem Item { get; init; }
+        public Node? Next { get; set; }
+    }
+
+    public void Add(TItem item)
+    {
+        var oldFirst = _first;
+        _first = new Node
+        {
+            Item = item,
+            Next = oldFirst
+        };
+
+        _index++;
+    }
+
+    public void RemoveLastNode()
+    {
+        if (IsEmpty()) return;
+        if (_index == 1)
+        {
+            _first = null;
+            return;
+        }
+
+        var nextNode = _first;
+        do
+        {
+            if (nextNode!.Next!.Next == null)
+            {
+                nextNode.Next = null;
+            }
+            nextNode = nextNode.Next;
+        } while (nextNode != null);
+    }
+
+    public bool IsEmpty() => _first == null;
+    public int Size() => _index;
+
+    public IEnumerator<TItem> GetEnumerator()
+    {
+        if (IsEmpty())
+            yield break;
+
+        var node = _first;
+        while (node != null)
+        {
+            yield return node.Item;
+            node = node.Next;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
---
+++ Ex/Ex.Fundamentals/1.3.19/Runner.cs
@@ -0,0 +1,38 @@
+﻿using GitGud.Ex.Fundamentals.Interfaces;
+
+namespace GitGud.Ex.Fundamentals._1._3._19;
+
+public class Runner : IRunner
+{
+    public string Identifier => "1.3.19";
+
+    public void Run()
+    {
+        var firstLinkedList = new LinkedListLastDeleter<int> { 1 };
+        firstLinkedList.RemoveLastNode();
+        Print(nameof(firstLinkedList), firstLinkedList);
+
+        var secondLinkedList = new LinkedListLastDeleter<int> { 1, 2 };
+        secondLinkedList.RemoveLastNode();
+        Print(nameof(secondLinkedList), secondLinkedList);
+
+        var thirdLinkedList = new LinkedListLastDeleter<int> { 1, 2, 3 };
+        thirdLinkedList.RemoveLastNode();
+        Print(nameof(thirdLinkedList), thirdLinkedList);
+
+        var fourthLinkedList = new LinkedListLastDeleter<int> { 1, 2, 3, 4 };
+        fourthLinkedList.RemoveLastNode();
+        Print(nameof(fourthLinkedList), fourthLinkedList);
+    }
+
+    private static void Print(string name, LinkedListLastDeleter<int> linkedList)
+    {
+        Console.WriteLine($"Printing contents of {name}");
+
+        var index = 0;
+        foreach (var item in linkedList)
+        {
+            Console.WriteLine($"{index++}. {item}");
+        }
+    }
+}
