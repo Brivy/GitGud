0c08e8de7c4ec4d5c9bf509bac421888bc5fa23f
Brian van Yperen <brianyperen@gmail.com>
2025-02-27 14:55:37 +0100

    1.3.21

---
+++ Ex/Ex.Fundamentals/1.3.21/LinkedListFinder.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+
+namespace GitGud.Ex.Fundamentals._1._3._21;
+
+public class LinkedListFinder : IEnumerable<string>
+{
+    private Node? _first;
+    private int _index;
+
+    private sealed class Node
+    {
+        public required string Item { get; init; }
+        public Node? Next { get; set; }
+    }
+
+    public void Add(string item)
+    {
+        var oldFirst = _first;
+        _first = new Node
+        {
+            Item = item,
+            Next = oldFirst
+        };
+
+        _index++;
+    }
+
+    public bool Find(string key)
+    {
+        if (IsEmpty())
+            return false;
+
+        for (var x = _first; x != null; x = x.Next)
+            if (x.Item == key) return true;
+
+        return false;
+    }
+
+    public bool IsEmpty() => _first == null;
+    public int Size() => _index;
+
+    public IEnumerator<string> GetEnumerator()
+    {
+        if (IsEmpty())
+            yield break;
+
+        var node = _first;
+        while (node != null)
+        {
+            yield return node.Item;
+            node = node.Next;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
---
+++ Ex/Ex.Fundamentals/1.3.21/Runner.cs
@@ -0,0 +1,22 @@
+﻿using GitGud.Ex.Fundamentals.Interfaces;
+
+namespace GitGud.Ex.Fundamentals._1._3._21;
+
+public class Runner : IRunner
+{
+    public string Identifier => "1.3.21";
+
+    public void Run()
+    {
+        var linkedList = new LinkedListFinder() { "a", "b", "ccc", "d", "e" };
+        string? input;
+        do
+        {
+            Console.Write("What do you want to search: ");
+            input = Console.ReadLine();
+        } while (string.IsNullOrWhiteSpace(input));
+
+        var found = linkedList.Find(input);
+        Console.WriteLine($"Was '{input}' found: {(found ? "YES" : "NO")}");
+    }
+}
