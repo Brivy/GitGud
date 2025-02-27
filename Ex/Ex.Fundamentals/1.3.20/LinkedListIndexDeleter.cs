affa7d123f8af2c402ddd0aa6478e7a422dac621
Brian van Yperen <brianyperen@gmail.com>
2025-02-27 14:41:44 +0100

    1.3.20

---
+++ Ex/Ex.Fundamentals/1.3.20/LinkedListIndexDeleter.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+
+namespace GitGud.Ex.Fundamentals._1._3._19;
+
+public class LinkedListIndexDeleter<TItem> : IEnumerable<TItem>
+{
+    private Node? _first;
+    private int _index;
+
+    private sealed class Node
+    {
+        public required TItem Item { get; init; }
+        public Node? Next { get; set; }
+    }
+
+    public void Add(TItem item)
+    {
+        var oldFirst = _first;
+        _first = new Node
+        {
+            Item = item,
+            Next = oldFirst
+        };
+
+        _index++;
+    }
+
+    public void RemoveAtIndex(int index)
+    {
+        if (IsEmpty()) return;
+        if (index == 0)
+        {
+            if (_index == 1) _first = null;
+            else _first = _first!.Next;
+            return;
+        }
+
+        var node = _first;
+        for (var i = 0; i < index - 1; i++)
+        {
+            if (node!.Next == null) return; // if you ever encounter a null, just quit
+            node = node.Next;
+        }
+
+        if (node!.Next == null) return; // safeguard for if you try to delete something right out of your index
+        node!.Next = node!.Next!.Next; // overwrite the node that you are supposed to delete
+    }
+
+    public bool IsEmpty() => _first == null;
+    public int Size() => _index;
+
+    public IEnumerator<TItem> GetEnumerator()
+    {
+        if (IsEmpty())
+            yield break;
+
+        var node = _first;
+        while (node != null)
+        {
+            yield return node.Item;
+            node = node.Next;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
---
+++ Ex/Ex.Fundamentals/1.3.20/Runner.cs
@@ -0,0 +1,35 @@
+﻿using GitGud.Ex.Fundamentals._1._3._19;
+using GitGud.Ex.Fundamentals.Interfaces;
+
+namespace GitGud.Ex.Fundamentals._1._3._20;
+
+public class Runner : IRunner
+{
+    public string Identifier => "1.3.20";
+
+    public void Run()
+    {
+        Console.WriteLine("The following linked list is available: ");
+        var linkedList = new LinkedListIndexDeleter<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        Print(linkedList);
+
+        Console.Write("Which index do you want to delete: ");
+        var indexNumberString = Console.ReadLine();
+        if (!string.IsNullOrWhiteSpace(indexNumberString) && int.TryParse(indexNumberString, out int indexNumber))
+        {
+            linkedList.RemoveAtIndex(indexNumber);
+        }
+
+        Console.WriteLine("Oh wow, you now have the following linked list: ");
+        Print(linkedList);
+    }
+
+    private static void Print(LinkedListIndexDeleter<int> linkedList)
+    {
+        var index = 0;
+        foreach (var item in linkedList)
+        {
+            Console.WriteLine($"{index++}. {item}");
+        }
+    }
+}
