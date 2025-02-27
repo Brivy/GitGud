83d1e6f87fdb0a9c95003275a32b45b0de994200
Brian van Yperen <brianyperen@gmail.com>
2025-02-27 13:11:28 +0100

    StackWithIndexer

---
+++ Ex/Ex.Fundamentals/1.3.15/Runner.cs
@@ -0,0 +1,42 @@
+﻿using GitGud.Ex.Fundamentals.Interfaces;
+
+namespace GitGud.Ex.Fundamentals._1._3._15;
+
+public class Runner : IRunner
+{
+    public string Identifier => "1.3.15";
+
+    public void Run()
+    {
+        var stack = new StackWithIndexer<string>();
+        var input = "This is the default string";
+
+        Console.Write("Give a string as input that will be stored in a stack, or use the default string: ");
+        var userInput = Console.ReadLine();
+        if (!string.IsNullOrWhiteSpace(userInput))
+        {
+            input = userInput;
+        }
+
+        var inputSegments = input.Split(' ');
+        foreach (var inputSegment in inputSegments)
+        {
+            stack.Push(inputSegment);
+        }
+
+        Console.WriteLine("Stack consists out of the following strings:");
+        var index = 0;
+        foreach (var item in stack)
+        {
+            Console.WriteLine($"{index++}. {item}");
+        }
+
+        Console.Write("Please enter a index number for which string you want to return: ");
+        var indexNumberString = Console.ReadLine();
+        if (!string.IsNullOrWhiteSpace(indexNumberString) && int.TryParse(indexNumberString, out int indexNumber))
+        {
+            var result = stack[indexNumber];
+            Console.WriteLine($"Your string is: {result}");
+        }
+    }
+}
---
+++ Ex/Ex.Fundamentals/1.3.15/StackWithIndexer.cs
@@ -0,0 +1,74 @@
+﻿using GitGud.DS.Stack.Interfaces;
+using System.Collections;
+
+namespace GitGud.Ex.Fundamentals._1._3._15;
+
+public class StackWithIndexer<TItem> : IStack<TItem>
+{
+    private Node? _first;
+    private int _index;
+
+    private sealed record Node
+    {
+        public required TItem Item { get; init; }
+        public Node? Next { get; init; }
+    }
+
+    public void Push(TItem item)
+    {
+        var oldFirst = _first;
+        _first = new Node
+        {
+            Item = item,
+            Next = oldFirst
+        };
+
+        _index++;
+    }
+
+    public TItem Pop()
+    {
+        if (IsEmpty()) throw new InvalidOperationException("Stack is empty");
+
+        var item = _first!.Item;
+        _first = _first.Next;
+        _index--;
+
+        return item;
+    }
+
+    public TItem this[int index]
+    {
+        get
+        {
+            var indexedNode = _first;
+            for (var i = 0; i < index; i++)
+            {
+                indexedNode = indexedNode?.Next ?? throw new InvalidOperationException("Index not found");
+            }
+
+            return indexedNode!.Item;
+        }
+    }
+
+    public int Size() => _index;
+    public bool IsEmpty() => _index == 0;
+
+    public IEnumerator<TItem> GetEnumerator()
+    {
+        if (IsEmpty())
+            yield break;
+
+        var node = _first;
+        while (node != null)
+        {
+            yield return node.Item;
+            node = node.Next;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
