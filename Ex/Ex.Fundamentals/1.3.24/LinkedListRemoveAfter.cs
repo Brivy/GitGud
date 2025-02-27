5e8cd809a3479746cb50695845fc8a3d328f4e91
Brian van Yperen <brianyperen@gmail.com>
2025-02-27 15:17:55 +0100

    1.3.24

---
+++ Ex/Ex.Fundamentals/1.3.24/LinkedListRemoveAfter.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+
+namespace GitGud.Ex.Fundamentals._1._3._24;
+
+public class LinkedListRemoveAfter : IEnumerable<string>
+{
+    private Node? _first;
+    private int _index;
+
+    private sealed class Node
+    {
+        public required string Item { get; init; }
+        public Node? Next { get; set; }
+    }
+
+    public void Add(string item)
+    {
+        var oldFirst = _first;
+        _first = new Node
+        {
+            Item = item,
+            Next = oldFirst
+        };
+
+        _index++;
+    }
+
+    public void RemoveAfter(string key)
+    {
+        if (IsEmpty())
+            return;
+
+        for (var x = _first; x != null; x = x.Next)
+        {
+            if (x.Item != key) continue;
+            if (x.Next == null || x.Next.Next == null) continue;
+            x.Next = x.Next.Next;
+        }
+    }
+
+    public bool IsEmpty() => _first == null;
+    public int Size() => _index;
+
+    public IEnumerator<string> GetEnumerator()
+    {
+        if (IsEmpty())
+            yield break;
+
+        var node = _first;
+        while (node != null)
+        {
+            yield return node.Item;
+            node = node.Next;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
---
+++ Ex/Ex.Fundamentals/1.3.24/Runner.cs
@@ -0,0 +1,36 @@
+﻿using GitGud.Ex.Fundamentals.Interfaces;
+
+namespace GitGud.Ex.Fundamentals._1._3._24;
+
+public class Runner : IRunner
+{
+    public string Identifier => "1.3.24";
+
+    public void Run()
+    {
+        var linkedList = new LinkedListRemoveAfter { "a", "b", "c", "d", "e", "f", "g" };
+        Console.WriteLine("The following linked list exists: ");
+        Print(linkedList);
+
+        string? input;
+        do
+        {
+            Console.Write("Which node neighbor do you want to destroy: ");
+            input = Console.ReadLine();
+        } while (string.IsNullOrWhiteSpace(input));
+
+        linkedList.RemoveAfter(input);
+
+        Console.WriteLine("This list remains: ");
+        Print(linkedList);
+    }
+
+    private static void Print(LinkedListRemoveAfter linkedList)
+    {
+        var index = 0;
+        foreach (var item in linkedList)
+        {
+            Console.WriteLine($"{index++}. {item}");
+        }
+    }
+}
